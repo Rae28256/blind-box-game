2b6ca66de131e1bc95415aa36afb6bf26abf7b47
7.6e <nnynna@163.com>
2026-07-06 23:09:03 +0800

    完成Page1跳转Page2

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
-document.querySelectorAll(".click-box").forEach(box => {
+const clickBoxes = document.querySelectorAll(".click-box");
+const page2 = document.getElementById("page2");
 
-    box.addEventListener("click", (e) => {
-
-        const id = e.target.dataset.id;
-
-        console.log("点击了盲盒：", id);
-
-        // 👉 切到第二页
-        document.getElementById("page1").classList.remove("active");
-        document.getElementById("page2").classList.add("active");
-
-        // 👉 更新缩略图
-        updateThumb(id);
-
+clickBoxes.forEach(box => {
+    box.addEventListener("click", () => {
+        page2.style.display = "block";
     });
-
 });
